4817f0e1a0952c3b5ea774899e05966d0cd2607a
laurent.allegre <laurent.allegre1@gmail.com>
2022-10-09 15:33:25 +0200

    route bdd

--- models/Model.class.php
+++ models/Model.class.php
@@ -4,7 +4,7 @@ abstract class Model{
     private static $pdo;
 
     private static function setBdd(){
-        self::$pdo = new PDO("mysql:host=localhost;dbname=squelettesite;charset=utf8", "root", "");
+        self::$pdo = new PDO("mysql:host=localhost;dbname=leNomDeVotreBdd;charset=utf8", "root", "");
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
